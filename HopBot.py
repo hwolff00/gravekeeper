1b39cf40cefc7d3cc72676a94f300aceecb5fbd1
hwolff00 <hwolff00@gmail.com>
2021-06-15 18:13:47 -0400

    Code is ugly but it works.

--- HopBot.py
+++ HopBot.py
@@ -1,10 +1,11 @@
 import asyncio
 from rctogether import bots, RestApiSession, messages, api
+from rctogether import WebsocketSubscription
 import pickle
 import re
 import random
+import datetime
 import time
-
 #-------------------------------------------------------------------------------
 # Home (Where HopBot starts.)
 Home = {"x": 6, "y": 1}
@@ -71,49 +72,54 @@ f"{Pronoun.capitalize()} {Past_Verb} the {Tech} {IN.lower()} {Second_Plural_Noun
 f"{tour['name']} was born out of {Tech} and {Past_Verb} due to {Bad_Tech}.",
 f"{Bad_Tech.capitalize()} {Past_Verb} {tour['name']} but it also {Second_Past_Verb} them."
 ]
-#
+
+COMMANDS = []
+#-------------------------------------------------------------------------------
+
 def story():
-    return f"Ah, yes I remember {tour['name']}! {random.choice(SCRIPTS)}"
+    return f"{random.choice(['Ah, yes I remember', 'Did you know', 'Have I ever told you about'])} {tour['name']}: {random.choice(SCRIPTS)}"
 
 def words_of_wisdom():
     return random.choice(WOW)
 
-print(story())
-print(words_of_wisdom())
+def response_handler(commands, pattern):
+    def handler(f):
+        commands.append((pattern, f))
+        return f
 
-#TODO make it so HopBot only speaks when mentioned
+    return handler
 
-# #dict_keys(['id', 'type', 'pos', 'emoji', 'direction', 'can_be_mentioned', 'app', 'name', 'display_name', 'message'])
-# async def main():
-#     async with RestApiSession() as session:
-#         for i in await bots.get(session):
-#             if i['emoji'] == "🧛":
-#                 print("Found Hop!")
-#                 # print(i["id"])
-#                 if i["pos"] == Home:
-#                     # await messages.send(session, i["id"], f"@**Hannah Wolff** Hiya!")
-#                     await bots.update(session, i["id"], tour["pos"])
-#                     print(tour["name"])
-#                     time.sleep(6)
-#                     await bots.update(session, i["id"], Home)
-#                 if i["pos"] != Home:
-#                     await bots.update(session, i["id"], Home)
-#
-#
-# asyncio.run(main())
+#-------------------------------------------------------------------------------
+#TODO make this code less insane
+#TODO add in input functionality
+
+async def main():
+    processed_message_dt = datetime.datetime.utcnow()
+    async for message in WebsocketSubscription():
+        for key, value in message.items():
+            if type(value) is dict and 'mentioned_entity_ids' in value.keys():
+                    if value['mentioned_entity_ids'] == [92348]:
+                        # print(message['message']['sent_at'])
+                        message_dt = datetime.datetime.strptime(message['message']['sent_at'], "%Y-%m-%dT%H:%M:%SZ")
+                        if message_dt <= processed_message_dt:
+                            print("Skipping old message")
+                            # print(message['person_name'])
+                        else:
+                            processed_message_dt = message_dt
+                            async with RestApiSession() as session:
+                                for bot in await bots.get(session):
+                                    if bot['emoji'] == "🧛":
+                                        print("Found Hop!")
+                                        # print(i["id"])
+                                        if bot["pos"] == Home:
+                                            await messages.send(session, bot["id"], f"@**{message['person_name']}** {story()}")
+                                            await bots.update(session, bot["id"], tour["pos"])
+                                            # print(story())
+                                            time.sleep(6)
+                                            await bots.update(session, bot["id"], Home)
+                                        if bot["pos"] != Home:
+                                            await bots.update(session, bot["id"], Home)
 
 
-#'@**HopBot (bot)** hi'
 
-# import asyncio
-# from rctogether import WebsocketSubscription
-#
-# async def main():
-#     async for message in WebsocketSubscription():
-#         for key, value in message.items():
-#             if type(value) is dict and 'mentioned_entity_ids' in value.keys():
-#                     if value['mentioned_entity_ids'] == [92348]:
-#                         print(f"@**{message['person_name']} Hiya!")
-#
-#
-# asyncio.run(main())
+asyncio.run(main())
